886620fe775c0d433c8fa5f481d359b5ee616d47
רועי גיסין <roygissin@h-MacBook-Pro-sl-rwy.local>
2024-11-28 11:04:27 +0200

    hw4

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,34 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        for(int i=0;i<sentences.length;i++){
+            for(int j=0;j<keywords.length;j++){
+                if(contains(sentences[i], keywords[j])){
+                    System.out.println(sentences[i]);
+                    break;
+                }
+            }
+        }
     }
+
+    public static boolean contains(String str1, String str2) {
+        if(str2.length()==0){
+            return true;}
+        if(str1.length()<str2.length()){
+            return false;
+        }
+         for(int i=0;i<=str1.length()-str2.length();i++){
+            int j;
+            for(j=0;j<str2.length();j++){
+                if(str1.charAt(i+j)!=str2.charAt(j)){
+                    break;
+                } 
+            } if(j==str2.length()){
+                return true;
+            }
+        }     
+        
+        return false;
+    
+} 
 }
